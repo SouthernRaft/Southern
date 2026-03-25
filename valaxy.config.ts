0a20415fe9f01989a1f554c7ab25046e7260b9d9
SouthernRaft <camellia065@outlook.com>
2026-03-25 14:37:20 +0800

    fix bug

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,16 +1,16 @@
 import type { UserThemeConfig } from 'valaxy-theme-yun'
 import { defineValaxyConfig } from 'valaxy'
 
-// add icons what you will need
 const safelist = [
   'i-ri-home-line',
 ]
 
-/**
- * User Config
- */
 export default defineValaxyConfig<UserThemeConfig>({
-  // site config see site.config.ts
+  // url: 'https://SouthernRaft.github.io/Southern/',
+
+  vite: {
+    base: '/Southern/',
+  },
 
   theme: 'yun',
 
